ab748b411bb9b1ceb17f5b46a0d5e1dae0bea9e5
taajheel <tajheel@gmail.com>
2024-12-17 17:55:48 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/taajheel/bolt/', // Replace with your repository name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
